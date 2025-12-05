39b915287b442acfddddd826a9ba0837b9a9658b
HyeonSu <ih9158228@gmail.com>
2025-12-05 15:02:21 +0900

    초기세팅2

---
+++ sichuation/src/main/java/com/web/sichuation/SichuationApplication.java
@@ -0,0 +1,13 @@
+package com.web.sichuation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SichuationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SichuationApplication.class, args);
+	}
+
+}
---
+++ sichuation/src/test/java/com/web/sichuation/SichuationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.sichuation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SichuationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
