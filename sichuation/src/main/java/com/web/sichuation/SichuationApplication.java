63dccdb3230515df5c49c569f733bfcc2c96b169
TMILY9526 <dnwn199@naver.com>
2025-12-05 15:32:59 +0900

    reset_all

--- sichuation/src/main/java/com/web/sichuation/SichuationApplication.java
+++ sichuation/src/main/java/com/web/sichuation/SichuationApplication.java
@@ -5,11 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SichuationApplication {
-    //hello
-    int a = 0;
-    int b = 1;
 	public static void main(String[] args) {
 		SpringApplication.run(SichuationApplication.class, args);
 	}
-//qq
 }
