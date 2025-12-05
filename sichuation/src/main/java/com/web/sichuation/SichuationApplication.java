21b9ce443f40bbdb07fc30f329ab7b683d06f73c
TMILY9526 <dnwn199@naver.com>
2025-12-05 15:11:03 +0900

    test

--- sichuation/src/main/java/com/web/sichuation/SichuationApplication.java
+++ sichuation/src/main/java/com/web/sichuation/SichuationApplication.java
@@ -6,6 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SichuationApplication {
 
+
+
+
 	public static void main(String[] args) {
 		SpringApplication.run(SichuationApplication.class, args);
 	}
