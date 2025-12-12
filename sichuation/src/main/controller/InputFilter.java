b93ea28166def1628bf1694377cc1cef240ad066
yoonmi <dbsal0327@gmail.com>
2025-12-12 15:56:12 +0900

    2025.12.12 (15:56)

--- sichuation/src/main/controller/InputFilter.java
+++ sichuation/src/main/controller/InputFilter.java
@@ -14,7 +14,7 @@ public class InputFilter {
                 "그냥", "좀", "약간", "혹시", "일단", "그리고", "근데",
                 "뭐냐면", "뭐랄까", "아무튼", "음", "어", "아",
                  "그..", "저기", "그러니까", "솔직히",
-                "진짜", "너무", "완전", "개"
+                "진짜", "너무", "완전", "개","저기","부탁"
         };
         for (String f : fillers) {
             result = result.replace(f, "");
@@ -40,7 +40,7 @@ public class InputFilter {
         }
 
         // 3) 금지어 제거
-        String[] banned = {"씨발", "ㅅㅂ", "fuck", "좆"};
+        String[] banned = {"씨발", "ㅅㅂ", "fuck", "좆","ㅈㄴ","존나","꺼져","ㄲㅈ"};
         for (String b : banned) {
             result = result.replace(b, "");
         }
